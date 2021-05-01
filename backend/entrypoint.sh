dfae935ae932f0c5600b1d59e415e33dc1cade3d
keeykeey <sh1n_fuj1to@icloud.com>
2021-05-02 03:04:08 +0900

    ディレクトリ構成変更

--- backend/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-django-admin startproject config .
-python3 manage.py startapp api
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py collectstatic --no-input
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
